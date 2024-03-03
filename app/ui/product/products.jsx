2c41ef64e373542e099ba6272c850f0f25ee2f6e
Javier Lopéz <jlopezhdez94@gmail.com>
2024-03-03 14:18:54 -0500

    actualizar producto

--- app/ui/product/products.jsx
+++ app/ui/product/products.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid'
+import { EnvelopeIcon, PhoneIcon,PlusIcon,MinusIcon } from '@heroicons/react/20/solid'
 
-const people = [
+const product = [
   {
-    name: 'Jane Cooper',
+    name: 'Carne de Res',
     title: 'Vuelo MiamiA23',
-    role: 'Beatriz',
+    role: 'Lia online',
     email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
+    telephone: '1850.00 LB',
     reserva: "24-2 / 28-2",
     hora_llegada: "",
     notas: "",
@@ -19,23 +19,21 @@ const people = [
 function products() {
   return (
     <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {people.map((person) => (
+      {product.map((product) => (
         <li
-          key={person.email}
+          key={product.name}
           className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
         >
           <div className="flex flex-1 flex-col p-3 bg-white rounded-xl">
-          <h4 className="mt-1 mb-4 text-sm font-semibold text-blue-500">Reserva</h4>
-            <h3 className="mt-2 text-sm font-medium text-gray-900">Cliente: {person.name}</h3>
-            <h3 className="mt-2 text-sm font-medium text-gray-900">Reserva: {person.reserva}</h3>
-            <h3 className="mt-2 text-sm font-medium text-gray-900">Telefono: {person.telephone}</h3>
+          <h4 className="mt-1 mb-4 text-sm font-semibold text-blue-500">{product.name}</h4>   
+            <h3 className="mt-2 text-sm font-medium text-gray-900">Precio: {product.telephone}</h3>
             <dl className="mt-1 flex flex-grow flex-col justify-between">
               <dt className="sr-only">Title</dt>
-              <dd className="text-sm mt-2 text-red-500">{person.title}</dd>
+              {/* <dd className="text-sm mt-2 text-red-500">{product.title}</dd> */}
               <dt className="sr-only">Role</dt>
               <dd className="mt-3">
                 <span className="inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                  {person.role}
+                  {product.role}
                 </span>
               </dd>
             </dl>
@@ -44,20 +42,20 @@ function products() {
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
                 <a
-                  href={`mailto:${person.email}`}
+                  href={`mailto:${product.email}`}
                   className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
-                  <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  Editar
+                  <PlusIcon className="h-5 w-5 text-green-600" aria-hidden="true" />
+                  Agregar
                 </a>
               </div>
               <div className="-ml-px flex w-0 flex-1">
                 <a
-                  href={`tel:${person.telephone}`}
+                  href={`tel:${product.telephone}`}
                   className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
-                  <PhoneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                Modificar
+                  <MinusIcon className="h-5 w-5 text-red-600" aria-hidden="true" />
+                Eliminar
                 </a>
               </div>
             </div>
