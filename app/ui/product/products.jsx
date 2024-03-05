05150a6a20a63973e3b49b73e52fb40bb445c035
Javier Lopéz <jlopezhdez94@gmail.com>
2024-03-05 11:35:18 -0500

    product

--- app/ui/product/products.jsx
+++ app/ui/product/products.jsx
@@ -15,7 +15,7 @@ const product = [
   },
 ]
 
-function products() {
+function Products() {
   return (
     <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5">
       {product.map((product) => (
@@ -62,4 +62,4 @@ function products() {
     </ul>
   )
 }
-export default products
+export default Products
