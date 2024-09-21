99701871b5a0f0e1d6a96ba6a844064fee723124
Javier López <jlopezhdez94@gmail.com>
2024-09-21 03:42:18 -0400

    asd

--- app/ui/envios/ultimos-envios.tsx
+++ app/ui/envios/ultimos-envios.tsx
@@ -1,9 +1,5 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import { Ingresar } from '@/app/lib/definitions';
-import Link from 'next/link';
-import Search from '../search.jsx';
 import Table from '@/app/ui/Envios_Componentes/table'
-import  {useSearchParams}  from 'next/navigation.js';
+
 
 export default async function Page({
   searchParams,
